55901598fe6248bcf8dc11c42684f5c76a9b75ac
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-13 13:04:58 +0600

    setup userform first step with lot works

---
+++ src/components/formUserDetailed.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class FormUserDetailed extends Component {
+
+    render () {
+         return (
+             <div>Hello form userdetails</div>
+         )
+    }
+
+}
+
+
+export default FormUserDetailed
--- src/components/userForm.jsx
+++ src/components/userForm.jsx
@@ -1,14 +1,75 @@
 import React, { Component } from 'react'
+import FormUserDetails from './formUserDetailed'
 
 export class userForm extends Component {
-  render() {
 
-    
-    return (
-      <div>
+    //default State
+
+    state = {
+        step:1,
+        firstName:'',
+        lastName:'',
+        email:'',
+        occupation:'',
+        city:'',
+        bio: ''
+
+
+    }
+
+        //Go to  next step
+
+    nextStep = () => {
+         const {step} = this.state;
+         this.setState( {
+             step:step +1
+         });
+         
+    }
+    //Go back prev step
+
+
+    prevStep = () => {
+        const {step} = this.state;
+        this.setState( {
+            step:step -1
+        });
         
-      </div>
-    )
+   }
+
+   //handle Filed Change
+
+   handleChange = input => e => {
+        this.setState({
+            [input]:e.target.value
+        });
+   }
+
+
+
+
+  render() {
+       const {step} = this.state;
+       const {firstName, lastName, email, occupation, city, bio} = this.state;
+       const values ={firstName, lastName, email, occupation, city, bio};
+
+       switch(step) {
+           case 1:
+           return (
+               <FormUserDetails
+               nextStep = {this.nextStep}
+               handleChange={this.handleChange}
+               values={values}
+               />
+           )
+           case 2: 
+           return <h1>Form PersonalDetails</h1>
+           case 3: 
+           return <h1>Confirm</h1>
+           case 4: 
+           return <h1>Succes</h1>
+       }
+
   }
 }
 
